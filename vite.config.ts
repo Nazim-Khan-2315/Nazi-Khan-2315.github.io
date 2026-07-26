1fe5d2effb6ec9566fd6149e61e7f6116af6c706
Nazim Khan <124751512+Nazim-Khan-2315@users.noreply.github.com>
2026-07-26 08:13:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/nazim-khan-2315.github.io/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
